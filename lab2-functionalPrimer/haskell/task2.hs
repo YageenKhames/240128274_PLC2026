2ea6790c9cf05fba4e4fa1ef2040d31f22f7e9ef
YageenKhames <240128274@aston.ac.uk>
2026-02-08 15:56:03 +0000

    Task 2

--- lab2-functionalPrimer/haskell/task2.hs
+++ lab2-functionalPrimer/haskell/task2.hs
@@ -1,5 +1,6 @@
+onePlusone = putStrLn("1 + 1 = 2")
 main = do
-    putStrLn "Welcome to the programme. Please enter your name"
-    num1 <- getLine
-    num2 <- getLine
-    putStrLn("this " ++ num1 ++ ", plus this" ++ num2 ++ "equals" )
+  putStrLn "Welcome to the programme. Please enter your name"
+  name <- getLine
+  putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+  onePlusone
